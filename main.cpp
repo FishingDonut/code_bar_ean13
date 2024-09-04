5b23c88a4a569131f363308a904894c9d2e42635
FishingDonut <106450510+FishingDonut@users.noreply.github.com>
2024-09-04 12:03:44 -0300

    integration code main to development

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     long long codigo_barra;
-    int d12, d11, d10, d9, d8, d7, d6, d5, d4, d3, d2, d1, d0;
+    int d12, d11, d10, d9, d8, d7, d6, d5, d4, d3, d2, d1, d0, total;
 
     cout << "Digite o Codigo de Barras." << endl;
     cin >> codigo_barra;
@@ -38,9 +38,59 @@ int main()
         codigo_barra /= 10;
         d12 = codigo_barra % 10;
 
-        cout << d0 << endl << d1 << endl<< d2 << endl<< d3 << endl<< d4 << endl<< d5 << endl<< d6 << endl<< d7 << endl<< d8 << endl<< d9 << endl<< d10 << endl << d11 << endl<< d12 << endl;
+        cout << d0 << endl
+             << d1 << endl
+             << d2 << endl
+             << d3 << endl
+             << d4 << endl
+             << d5 << endl
+             << d6 << endl
+             << d7 << endl
+             << d8 << endl
+             << d9 << endl
+             << d10 << endl
+             << d11 << endl
+             << d12 << endl;
 
-    } else {
+        d12 *= 1;
+        d11 *= 3;
+        d10 *= 1;
+        d9 *= 3;
+        d8 *= 1;
+        d7 *= 3;
+        d6 *= 1;
+        d5 *= 3;
+        d4 *= 1;
+        d3 *= 3;
+        d2 *= 1;
+        d1 *= 3;
+
+        total = d12 + d11 + d10 + d9 + d8 + d7 + d6 + d5 + d4 + d3 + d2 + d1;
+
+        int digito_verificador = (((total / 10) + 1) * 10) - total;
+
+        cout << "Total:\t" << total << endl;
+        cout << "Total Dividido:\t" << total / 10 << endl;
+        cout << "Total Dividido  soma 1:\t" << (total / 10) + 1 << endl;
+        cout << "Multiplicado por 10 Total Dividido soma 1:\t" << ((total / 10) + 1) * 10 << endl;
+        cout << "Digito verificador:\t" << digito_verificador << endl;
+
+        if (digito_verificador >= 10)
+        {
+            digito_verificador = 0;
+        }
+
+        if (digito_verificador == d0)
+        {
+            cout << "Sim, esta correto o codigo de barra" << endl;
+        }
+        else
+        {
+            cout << "Nao esta correto o codigo de barra" << endl;
+        }
+    }
+    else
+    {
         cout << "Número de dígitos inválidos. Deve conter ecxatamente 13 dígitos." << endl;
         return 1;
     }
