885950f97e1b72c2b8a5f9bee3295915a96fe745
FishingDonut <106450510+FishingDonut@users.noreply.github.com>
2024-09-04 17:10:05 -0300

    add commits and format

--- main.cpp
+++ main.cpp
@@ -4,14 +4,19 @@ using namespace std;
 
 int main()
 {
+    // define as variaveis
     long long codigo_barra;
     int d12, d11, d10, d9, d8, d7, d6, d5, d4, d3, d2, d1, d0, total, digito_verificador;
 
+    // passa a instrução ao usuario
     cout << "Digite o Codigo de Barras." << endl;
     cin >> codigo_barra;
 
-    if (codigo_barra > 0 and codigo_barra <= 9999999999999LL) {
+    // verifica o tamanho do codigo de barras passado
+    if (codigo_barra > 0 and codigo_barra <= 9999999999999LL)
+    {
 
+        // Usa o modulo para pegar o ultimo digito da direita e divide a operaçao em 10 para retirar o ultimo digito da direita
         d0 = codigo_barra % 10;
         codigo_barra /= 10;
         d1 = codigo_barra % 10;
@@ -38,6 +43,7 @@ int main()
         codigo_barra /= 10;
         d12 = codigo_barra % 10;
 
+        // multiplica por 3 os numeros impares e os pares por 1.
         d12 *= 1;
         d11 *= 3;
         d10 *= 1;
@@ -51,14 +57,17 @@ int main()
         d2 *= 1;
         d1 *= 3;
 
+        // soma os digitos menos o digito verificador d0
         total = d12 + d11 + d10 + d9 + d8 + d7 + d6 + d5 + d4 + d3 + d2 + d1;
 
+        // faz o calculo para obter o digito verificador
         digito_verificador = total;
         digito_verificador /= 10;
         digito_verificador += 1;
         digito_verificador *= 10;
         digito_verificador -= total;
 
+        // verifica se e multiplo de 10
         if (digito_verificador >= 10)
         {
             digito_verificador = 0;
